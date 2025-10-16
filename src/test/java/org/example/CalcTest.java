40d43bae153ea4a11ce2e794a5cf78064d0e49f5
SijiaLiu111 <2233682801@qq.com>
2025-10-16 18:44:37 +0800

    Fix subtract method, tests should pass

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -4,11 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class CalcTest {
-  Calc c = new Calc();
 
     @Test
-    void testAddition() {
-        assertEquals(4, c.add(2,2));
+    public void testSubtraction() {
+        assertEquals(10, )
     }
-
 }
